8efb42b051e714f90338736285cb4c3f1900af74
beyoungV <43118622+beyoungV@users.noreply.github.com>
2020-08-11 17:05:52 +0800

    Update my.go

--- somethings/my.go
+++ somethings/my.go
@@ -1,1 +1,33 @@
-
+set GO111MODULE=on
+set GOARCH=amd64
+set GOBIN=
+set GOCACHE=C:\Users\wicky\AppData\Local\go-build
+set GOENV=C:\Users\wicky\AppData\Roaming\go\env
+set GOEXE=.exe
+set GOFLAGS=
+set GOHOSTARCH=amd64
+set GOHOSTOS=windows
+set GOINSECURE=
+set GONOPROXY=
+set GONOSUMDB=
+set GOOS=windows
+set GOPATH=E:\Go-workspace
+set GOPRIVATE=
+set GOPROXY=https://goproxy.cn
+set GOROOT=c:\go
+set GOSUMDB=sum.golang.org
+set GOTMPDIR=
+set GOTOOLDIR=c:\go\pkg\tool\windows_amd64
+set GCCGO=gccgo
+set AR=ar
+set CC=gcc
+set CXX=g++
+set CGO_ENABLED=1
+set GOMOD=NUL
+set CGO_CFLAGS=-g -O2
+set CGO_CPPFLAGS=
+set CGO_CXXFLAGS=-g -O2
+set CGO_FFLAGS=-g -O2
+set CGO_LDFLAGS=-g -O2
+set PKG_CONFIG=pkg-config
+set GOGCCFLAGS=-m64 -mthreads -fmessage-length=0 -fdebug-prefix-map=C:\Users\wicky\AppData\Local\Temp\go-build716206338=/tmp/go-build -gno-record-gcc-switches
